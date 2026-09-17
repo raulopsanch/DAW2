82557eff338e3bf42ed85e5aef64226ae09ef066
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-09-17 10:57:29 +0200

    hola 1.1

--- Desarrollo web en entorno cliente/tema_01/actividades/restaurant.js
+++ Desarrollo web en entorno cliente/tema_01/actividades/restaurant.js
@@ -2,9 +2,7 @@ let precio = parseFloat(prompt("Introduce el importe de la cuenta: "))
 
 let propina = parseFloat(prompt("Introduce el porcentaje de propina: "))
 
-
 let cuenta = precio + (precio * propina / 100)
-
 
 let respuesta = confirm("¿Quieres mostrar la cuenta?")
 
