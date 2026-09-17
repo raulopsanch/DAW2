08f6ad696442c3fea32b049935df3eec3fb82fd4
raulopsanch <raul_lopez.sanchez@outlook.es>
2026-09-17 18:20:23 +0200

    inicio php

---
+++ Desarrollo web en entorno servidor/Tema_02/prueba/2026.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+	<p>
+		Este es el curso de php de 2026
+	</p>
+        <?php
+		echo "Hola mundo"
+	?>
+    </body>
+</html>
