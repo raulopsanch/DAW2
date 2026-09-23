750838da6cc5cdd01c9e97ea3042076db06d84d0
raulopsanch <raul_lopez.sanchez@outlook.es>
2026-09-23 22:21:21 +0200

    ejercicios_2_2-6

--- Desarrollo web en entorno cliente/tema_02/ejercicios_02/miScript.js
+++ Desarrollo web en entorno cliente/tema_02/ejercicios_02/miScript.js
@@ -1,5 +1,52 @@
+'use strict'
+
+
 function Ej1() {
   let num1 = 5;
   let resultado = (num1 = 3); // ¿Cuánto vale aquí ‘resultado’?
   resultado = num1 == 3; // ¿Cuánto vale aquí ‘resultado’?
+}
+
+
+function Ej2() {
+  let v1 = 2;
+  let v2 = 4;
+  let v3 = 5;
+
+  let resultado = v1 + v2 * v3;
+}
+
+
+function Ej3() {
+  let a = 5;
+  a /= 2;
+  a += 1;
+  a --;
+  a++;
+}
+
+
+function Ej4() {
+  let num1 = parseInt(prompt("Introduce el primer número: "));
+  let num2 = parseInt(prompt("Introduce el segundo número: "));
+  let num3 = parseInt(prompt("Introduce el tercer número: "));
+ 
+  let resultado_suma = num1 + num2 + num3;
+  let media = resultado_suma / 3;
+  
+  console.log(`El resultado de la suma es: ${resultado_suma}`);
+  console.log(`El resultado de la media es: ${media}`);
+}
+
+
+function Ej5() {
+  let millas = parseInt(prompt("Introduce el primer número: "));
+
+  let metros = millas * 1609.34;
+
+}
+
+
+function Ej6() {
+
 }
