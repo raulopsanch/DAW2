0b6f7d3272ea766c4225f268912384f33d5cdf92
raulopsanch <149415897+raulopsanch@users.noreply.github.com>
2026-09-23 12:33:52 +0200

    format document with... prettier

--- Desarrollo web en entorno cliente/tema_02/ejercicios_02/ejercicio_01/ej01.js
+++ Desarrollo web en entorno cliente/tema_02/ejercicios_02/ejercicio_01/ej01.js
@@ -1,5 +1,5 @@
 function Ej1() {
-    let num1 = 5;
-    let resultado = num1 = 3; // ¿Cuánto vale aquí ‘resultado’?
-    resultado = num1 == 3; // ¿Cuánto vale aquí ‘resultado’?
+  let num1 = 5;
+  let resultado = (num1 = 3); // ¿Cuánto vale aquí ‘resultado’?
+  resultado = num1 == 3; // ¿Cuánto vale aquí ‘resultado’?
 }
