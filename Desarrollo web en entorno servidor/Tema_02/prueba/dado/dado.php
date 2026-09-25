cac779bcb5f7b322f6443aa4acaa0069dbceecde
raulopsanch <raul_lopez.sanchez@outlook.es>
2026-09-25 21:26:28 +0200

    php - anteproyecto

--- Desarrollo web en entorno servidor/Tema_02/prueba/dado/dado.php
+++ Desarrollo web en entorno servidor/Tema_02/prueba/dado/dado.php
@@ -6,13 +6,6 @@
         <title>Document</title>
     </head>
     <body>
-        <img src = "dado_1.jpg" width="50" height="50"/>
-        <img src = "dado_2.jpg" width="50" height="50"/>
-        <img src = "dado_3.jpg" width="50" height="50"/>
-        <img src = "dado_4.jpg" width="50" height="50"/>
-        <img src = "dado_5.jpg" width="50" height="50"/>
-        <img src = "dado_6.jpg" width="50" height="50"/>
-        <br />
         <?php
             $dado1 = 1;
             $dado2 = rand (1, 6);
@@ -21,12 +14,8 @@
 
             $imagenes = ["dado_1.jpg", "dado_2.jpg", "dado_3.jpg", "dado_4.jpg", "dado_5.jpg", "dado_6.jpg"];
 
-            if ($dado1 == 1 || $dado2 == 1) {
-                $imgDadoUno = $imagenes[0];
-                echo "<img src = '$imgDadoUno' width='50' height='50' />";
-                $imgDadoDos = $imagenes[0];
-                echo "<img src = '$imgDadoUno' width='50' height='50' />";
-            }
+            echo "<img src = " . $imagenes[dado_1];
+            
             echo "<br />Dado2: " . $dado2;
             echo "<br />" . $mensaje;
         ?>
---
+++ Desarrollo web en entorno servidor/Tema_02/prueba/menu/menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <<?php
+            echo "<nav>
+                <p>Bienvenido a mi página web</p>
+                <p>Nombre: Raúl López</p>
+                <p>2ª Desarrollo de Aplicaciones Web</p>
+            </nav>";
+        ?>
+    </body>
+</html>
